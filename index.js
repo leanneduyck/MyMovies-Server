ea5f22286464ab8fb24cbd5e1d83f05d7a440ab7
Leanne <154445685+leanneduyck@users.noreply.github.com>
2024-03-26 17:42:41 -0700

    updated index.js, ran endpoints

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // require express, also morgan
 const express = require("express"),
-  bodyParser = require("body-parser"),
+  //bodyParser = require("body-parser"),
   uuid = require("uuid");
 const morgan = require("morgan");
 const nodemon = require("nodemon");
@@ -10,8 +10,9 @@ require("dotenv").config();
 //(fs = require("fs")), (path = require("path"));
 
 const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// refuses to run in Postman when I include bodyParser
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: true }));
 
 //import mongoose and models
 const mongoose = require("mongoose");
@@ -25,11 +26,8 @@ const Directors = Models.Director;
 // connects to database so can do crud on documents
 mongoose
   .connect(
-    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster1.lx41vnw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1`
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster1.lx41vnw.mongodb.net/MyMovies?retryWrites=true&w=majority&appName=Cluster1`
   )
-  //.connect("mongodb://localhost:27017/MyMovies")
-  //useNewUrlParser: true,
-  //useUnifiedTopology: true,
   .then(() => {
     console.log("Connected to MongoDB.");
   })
@@ -60,7 +58,7 @@ app.get("/movies", async (req, res) => {
 });
 
 // 2. READ, returns one document via movie title
-app.get("movies/:Title", async (req, res) => {
+app.get("/movies/:Title", async (req, res) => {
   // pass parameter of title to find movie
   await Movies.findOne({ Title: req.params.Title })
     // confirmation response to client with movie document
@@ -75,7 +73,7 @@ app.get("movies/:Title", async (req, res) => {
 });
 
 // 3. READ, return data by genre
-app.get("movies/:Genre/:Name", async (req, res) => {
+app.get("/movies/Genre/:Name", async (req, res) => {
   // pass parameter of genre name to find all movie documents w/that genre
   await Movies.findOne({ GenreName: req.params.GenreName })
     // confirmation response to client with movie documents w/passed genre
@@ -90,7 +88,7 @@ app.get("movies/:Genre/:Name", async (req, res) => {
 });
 
 // 4. READ, return data by director
-app.get("movies/:Director/:Name", async (req, res) => {
+app.get("/movies/Director/:Name", async (req, res) => {
   // pass parameter of director name to find all movie documents w/that director
   await Movies.findOne({ DirectorName: req.params.DirectorName })
     // confirmation response to client with movie documents w/passed director
@@ -107,7 +105,7 @@ app.get("movies/:Director/:Name", async (req, res) => {
 // 5. CREATE, register new user, status 201 created, 400 bad request, 500 server error
 app.post("/users", async (req, res) => {
   // checks if user already exists
-  await Users.findOne({ Username: req.body.Username }).then((user) => {
+  await Users.findOne({ Name: req.body.Username }).then((user) => {
     if (user) {
       return res.status(400).send(req.body.Username + "User already exists.");
     } else {
@@ -213,227 +211,11 @@ app.delete("/users/:Username", async (req, res) => {
     });
 });
 
-// code from previous exercise - unnecessary now w/mongodb+mongoose???
-
-// let topMovies = [
-//   {
-//     Title: "Harry Potter and the Sorcerer's Stone",
-//     Release: "2001",
-//     Genre: "Adventure, Fantasy",
-//     Description:
-//       "An orphaned boy enrolls in a school of wizardry, where he learns the truth about himself, his family, and the terrible evil that haunts the magical world.",
-//     Rating: "PG",
-//     "Img URL":
-//       "https://www.imdb.com/title/tt0241527/mediaviewer/rm2105413120/?ref_=tt_ov_i",
-//     Director: {
-//       Name: "Chris Columbus",
-//       DOB: "09-10-1958",
-//       DOD: "N/A",
-//       Description:
-//         "Born in Pennsylvania and raised in Ohio, Chris Columbus was first inspired to make movies after seeing 'The Godfather' at age 15.",
-//     },
-//   },
-//   {
-//     Title: "Harry Potter and the Chamber of Secrets",
-//     Release: "2002",
-//     Genre: "Adventure, Fantasy",
-//     Description:
-//       "An ancient prophecy seems to be coming true when a mysterious presence begins stalking the corridors of a school of magic and leaving its victims paralyzed.",
-//     Rating: "PG",
-//     "Img URL":
-//       "https://www.imdb.com/title/tt0295297/mediaviewer/rm3790637825/?ref_=tt_ov_i",
-//     Director: {
-//       Name: "Chris Columbus",
-//       DOB: "09-10-1958",
-//       DOD: "N/A",
-//       Description:
-//         "Born in Pennsylvania and raised in Ohio, Chris Columbus was first inspired to make movies after seeing 'The Godfather' at age 15.",
-//     },
-//   },
-//   {
-//     Title: "Harry Potter and the Prisoner of Azkaban",
-//     Release: "2004",
-//     Genre: "Adventure, Fantasy",
-//     Description:
-//       "Harry Potter, Ron, and Hermione return to Hogwarts School of Witchcraft and Wizardry for their third year of study, where they delve into the myster surrounding an escaped prisoner who poses a dangerous threat to the young wizard.",
-//     Rating: "PG",
-//     "Img URL":
-//       "https://www.imdb.com/title/tt0304141/mediaviewer/rm3241184256/?ref_=tt_ov_i",
-//     Director: {
-//       Name: "Alfonso Cuaron",
-//       DOB: "11-28-1961",
-//       DOD: "N/A",
-//       Description:
-//         "Alfonso Cuaron Orozco was born in Mexico City, Mexico. From an early age, he yearned to be either a film director or an astronaut.",
-//     },
-//   },
-//   {
-//     Title: "Harry Potter and the Goblet of Fire",
-//     Release: "2005",
-//     Genre: "Adventure, Fantasy",
-//     Description:
-//       "Harry Potter finds himself competing in a hazardous tournament between rival schools of magic, but he is distracted by recurring nightmares.",
-//     Rating: "PG-13",
-//     "Img URL":
-//       "https://www.imdb.com/title/tt0330373/mediaviewer/rm436509952/?ref_=tt_ov_i",
-//     Director: {
-//       Name: "Mike Newell",
-//       DOB: "03-28-1942",
-//       DOD: "N/A",
-//       Description:
-//         "Mike Newell was born on 28 March 1942 in St. Albans, Hertfordshire, UK. He is married to Bernice Stegers. They have three children.",
-//     },
-//   },
-//   {
-//     Title: "Harry Potter and the Order of the Pheonix",
-//     Release: "2007",
-//     Genre: "Adventure, Fantasy",
-//     Description:
-//       "With their warning about Lord Voldemort's return scoffed at, Harry and Dumbledore are targeted by the Wizard authorities as an authoritarian bureaucrat slowly seizes power at Hogwarts.",
-//     Rating: "PG-13",
-//     "Img URL":
-//       "https://www.imdb.com/title/tt0373889/mediaviewer/rm3414694144/?ref_=tt_ov_i",
-//     Director: {
-//       Name: "David Yates",
-//       DOB: "10-08-1963",
-//       DOD: "N/A",
-//       Description:
-//         "David Yates was born on 8 October 1963 in St. Helens, Merseyside, UK. He directed several HP films.",
-//     },
-//   },
-//   {
-//     Title: "Harry Potter and the Half-Blood Prince",
-//     Release: "2009",
-//     Genre: "Adventure, Fantasy",
-//     Description:
-//       "As Harry Potter begins his sixth year at Hogwarts, he discovers an old book marked as 'the property of the Half-Blood Prince' and begins to learn more about Lord Voldemort's dark past.",
-//     Rating: "PG",
-//     "Img URL":
-//       "https://www.imdb.com/title/tt0417741/mediaviewer/rm282560512/?ref_=tt_ov_i",
-//     Director: {
-//       Name: "David Yates",
-//       DOB: "10-08-1963",
-//       DOD: "N/A",
-//       Description:
-//         "David Yates was born on 8 October 1963 in St. Helens, Merseyside, UK. He directed several HP films.",
-//     },
-//   },
-//   {
-//     Title: "Harry Potter and the Deathly Hallows: Part 1",
-//     Release: "2010",
-//     Genre: "Adventure, Fantasy",
-//     Description:
-//       "As Harry, Ron, and Hermione race against time and evil to destroy the Horcruxes, they uncover the existence of the three most powerful objects in the wizarding world: the Deathly Hallows.",
-//     Rating: "PG-13",
-//     "Img URL":
-//       "https://www.imdb.com/title/tt0926084/mediaviewer/rm706774528/?ref_=tt_ov_i",
-//     Director: {
-//       Name: "David Yates",
-//       DOB: "10-08-1963",
-//       DOD: "N/A",
-//       Description:
-//         "David Yates was born on 8 October 1963 in St. Helens, Merseyside, UK. He directed several HP films.",
-//     },
-//   },
-//   {
-//     Title: "Harry Potter and the Deathly Hallows: Part 2",
-//     Release: "2011",
-//     Genre: "Adventure, Fantasy",
-//     Description:
-//       "Harry, Ron, and Hermione search for Voldemort's remaining Horcruxes in their effort to destroy the Dark Lord as the final battle rages on at Hogwarts.",
-//     Rating: "PG-13",
-//     "Img URL":
-//       "https://www.imdb.com/title/tt1201607/mediaviewer/rm3179585537/?ref_=tt_ov_i",
-//     Director: {
-//       Name: "David Yates",
-//       DOB: "10-08-1963",
-//       DOD: "N/A",
-//       Description:
-//         "David Yates was born on 8 October 1963 in St. Helens, Merseyside, UK. He directed several HP films.",
-//     },
-//   },
-//   {
-//     Title: "The Lord of the Rings: The Fellowship of the Ring",
-//     Release: "2001",
-//     Genre: "Adventure, Fantasy",
-//     Description:
-//       "A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-Earth from the Dark Lord Sauron.",
-//     Rating: "PG-13",
-//     "Img URL":
-//       "https://www.imdb.com/title/tt0120737/mediaviewer/rm3592958976/?ref_=tt_ov_i",
-//     Director: {
-//       Name: "Peter Jackson",
-//       DOB: "10-31-1961",
-//       DOD: "N/A",
-//       Description:
-//         "Sir Peter Jackson made history with the LOTR trilogy, becoming the first person to direct three major feature films simultaneously.",
-//     },
-//   },
-//   {
-//     Title: "The Lord of the Rings: The Two Towers",
-//     Release: "2002",
-//     Genre: "Adventure, Fantasy",
-//     Description:
-//       "While Frodo and Sam edge closer to Mordor with the help of the shifty Gollum, the divided Fellowhip makes a stand against Sauron's new ally, Saruman, and his hordes of Isengard.",
-//     Rating: "PG-13",
-//     "Img URL":
-//       "https://www.imdb.com/title/tt0167261/mediaviewer/rm306845440/?ref_=tt_ov_i",
-//     Director: {
-//       Name: "Peter Jackson",
-//       DOB: "10-31-1961",
-//       DOD: "N/A",
-//       Description:
-//         "Sir Peter Jackson made history with the LOTR trilogy, becoming the first person to direct three major feature films simultaneously.",
-//     },
-//   },
-//   {
-//     Title: "The Lord of the Rings: The Return of the King",
-//     Release: "2003",
-//     Genre: "Adventure, Fantasy",
-//     Description:
-//       "Gandalf and Aaragorn lead the World of Men against Sauron's army to draw his gaze from Frodo and Sam as they approach Mt. Doom with the One Ring.",
-//     Rating: "PG-13",
-//     "Img URL":
-//       "https://www.imdb.com/title/tt0167260/mediaviewer/rm584928512/?ref_=tt_ov_i",
-//     Director: {
-//       Name: "Peter Jackson",
-//       DOB: "10-31-1961",
-//       DOD: "N/A",
-//       Description:
-//         "Sir Peter Jackson made history with the LOTR trilogy, becoming the first person to direct three major feature films simultaneously.",
-//     },
-//   },
-// ];
-
-// // get request to return json, endpoint /movies
-// app.get("/movies", (req, res) => {
-//   res.json(topMovies);
-// });
-
-// // get request returns default text, endpoint /
-// app.get("/", (req, res) => {
-//   res.send("This is a list of great movies.");
-// });
-
-// // gets documentation.html file from public folder instead of using modules (express.static)
-// app.use("/documentation.html", express.static("public"));
-
-// // uses morgan to log user requests
-// app.use(morgan("combined", { stream: accessLogStream }));
-
-// // error handling
-// app.use((err, req, res, next) => {
-//   consolse.error(err.stack);
-//   res.status(500).send("This is an error message.");
-// });
-
-// listens to port
-
 // access documentation.html using express.static
 
 app.use("/documentation", express.static("public"));
 
 // listen on port
-app.listen(8888, () => {
-  console.log("Listening on Port 8888.");
+app.listen(5050, () => {
+  console.log("Listening on Port 5050.");
 });
